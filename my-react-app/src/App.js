3381ec727e4174be167ec5f522c1936a687f2982
Vovarama1992 <vovayhh9988@gmail.com>
2024-04-16 15:01:45 +0300

    render

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,8 +2,16 @@ import { useState, useRef, useEffect, forwardRef } from 'react';
 import './App.css';
 
 function App() {
+  const [orders, setOrders] = useState([]);
+  const [products, setProducts] = useState([]);
   const refFirstRev = useRef(null);
   const refSecondRev = useRef(null);
+
+  useEffect(() => {
+    askProducts().then(results => setProducts(results))
+
+
+  }, [])
   
 
   useEffect(() => {
@@ -23,7 +31,12 @@ function App() {
         <Rev ref={refFirstRev} />
         <Rev ref={refSecondRev} />
       </div>
-     <OrderList></OrderList>
+     <OrderList orders={orders}></OrderList>
+     {products.map((pr, index) => {
+      const {image_url, title, description, price} = pr;
+      return (<ProductLook imgUrl={image_url} title={title} 
+        description={description} price={price} key={index}/>)
+     })}
       
        
     </div>
@@ -33,15 +46,40 @@ function App() {
 const Rev = forwardRef(({}, ref) => {
   return <div className="rev" ref={ref}></div>;
 });
-function  OrderList() {
+function OrderList({ orders }) {
   return (
-    <div className="orderList"></div>
+    <div className="orderList">
+      <div className="orderedText">Добавленные товары</div>
+      {orders.length > 0 &&
+        orders.map((order, index) => (
+          <div key={index} className="order">
+            {order.productName} x{order.count} {order.price}P
+          </div>
+        ))}
+        <input className="field" type="tel"
+  pattern="[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}"
+  placeholder="+7 (___) ___ __-__"
+ 
+  ></input>
+  
+<button className="field">Заказать</button>
+    </div>
+  );
+}
+function ProductLook({ imgUrl, title, description, price}) {
+  return (
+    <div className="product">
+      <img src={imgUrl}></img><br></br>
+      {title}<br></br>
+      {description}<br></br>
+      Цена: {price}P
+    </div>
   )
 }
 
 async function askRev() {
   try {
-    const res = await fetch('http://o-complex.com:1337/reviews', { method: 'GET' });
+    const res = await fetch('http://o-complex.com:1337/reviews', { method: 'GET'});
     if (!res.ok) {
       throw new Error("Fetch error");
     }
@@ -55,5 +93,13 @@ async function askRev() {
     return "not Found";
   }
 }
+
+async function askProducts(page = 1, size = 6) {
+  const res = await fetch(`http://o-complex.com:1337/products?page=${page}&page_size=${size}`);
+  const json = await res.json();
+  return json.products;
+}
+
+
 
 export default App;
